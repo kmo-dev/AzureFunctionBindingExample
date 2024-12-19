ed56e203d2641f76d5c86298eb5f4a6b2f5af6fe
klemensmorbe <klemens@morbe.online>
2024-12-19 23:56:57 +0100

    fixed logging

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Azure.Functions.Worker;
 
-var builder = FunctionsApplication.CreateBuilder(args);
-
-builder.ConfigureFunctionsWebApplication();
-
-builder.Services.ConfigureFunctionsApplicationInsights();
-
-builder.Build().Run();
+FunctionsApplication.CreateBuilder(args)
+.ConfigureFunctionsWebApplication()
+.Build().Run();
